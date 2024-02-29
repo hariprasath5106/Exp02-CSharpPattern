49952d224ae4a7fbce3caf254cd60e71a80fbe3e
hariprasath5106 <hariprasath.10c@gmail.com>
2024-02-29 09:23:36 +0530

    first commit

--- obj/Debug/net6.0/Exp02-CSharpPattern.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exp02-CSharpPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exp02-CSharpPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e19927dd3429e1d64cc118c96748a566a44839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b71dc3d1875a812afab575b92d131964570367f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exp02-CSharpPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exp02-CSharpPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
